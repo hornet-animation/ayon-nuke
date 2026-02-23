3801bb6844f768f995aac6e15a3816b76b98fea9
GitHub Action <action@github.com>
2026-02-23 23:09:37 +0000

    CI bump version to 0.3.7-hornet.29

--- client/ayon_nuke/version.py
+++ client/ayon_nuke/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'nuke' version."""
-__version__ = "0.3.7-hornet.28"
+__version__ = "0.3.7-hornet.29"
--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 name = "nuke"
 title = "Nuke"
 
-version = "0.3.7-hornet.28"
+version = "0.3.7-hornet.29"
 
 app_host_name = "nuke"
 client_dir = "ayon_nuke"
