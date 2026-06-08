f0153c8ec1f46b7b5d43423421426459670b9f2c
GitHub Action <action@github.com>
2026-06-08 16:13:25 +0000

    CI bump version to 0.4.0-hornet.46

--- client/ayon_nuke/version.py
+++ client/ayon_nuke/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'nuke' version."""
-__version__ = "0.4.0-hornet.45"
+__version__ = "0.4.0-hornet.46"
--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 name = "nuke"
 title = "Nuke"
 
-version = "0.4.0-hornet.45"
+version = "0.4.0-hornet.46"
 
 app_host_name = "nuke"
 client_dir = "ayon_nuke"
